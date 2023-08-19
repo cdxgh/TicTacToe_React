098bf37cf3d6c6762436fe5321945fd7041c1f61
AbdulQuadir <96329368+cdxgh@users.noreply.github.com>
2023-08-19 10:43:46 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import Board from "./TicToegame/Board";
+
+function App() {
+  return (
+    <div className="App">
+      <Board />
+    </div>
+  );
+}
+export default App;
---
+++ src/TicToegame/Square.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Square = () => {
+  return (
+    <div
+      style={{
+        border: "1px solid",
+        height: "100px",
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center"
+      }}
+      className="square"
+    >
+      <h5>O</h5>
+    </div>
+  );
+};
+export default Square;
